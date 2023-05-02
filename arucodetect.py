f07680259dd5c97afe2f7c7411c69e64da499ecb
gravity0717 <1357vvv@naver.com>
2023-05-02 10:45:08 +0900

    camera pose at origin point

--- arucodetect.py
+++ arucodetect.py
@@ -1,11 +1,27 @@
 import cv2
 import cv2.aruco as aruco 
-
-ARUCO_DICT =  {
-    "DICT_4X4_50" : cv2.aruco.DICT_4X4_50,
-    "DICT_5X5_50" : cv2.aruco.DICT_5X5_50,
-    "DICT_5X5_100" : cv2.aruco.DICT_5X5_100
-    
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+#	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+#	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+#	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+#	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
 }
 
 def aruco_display (corners, ids ,rejected, image):
@@ -14,7 +30,7 @@ def aruco_display (corners, ids ,rejected, image):
 
         for (markerCorner, markerID) in zip(corners, ids):
             corners = markerCorner.reshape((4,2))
-            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            (topRight, bottomRight, bottomLeft, topLeft) = corners
 
             topRight = (int(topRight[0]),int(topRight[1]))
             bottomRight = (int(bottomRight[0]),int(bottomRight[1]))
@@ -28,29 +44,36 @@ def aruco_display (corners, ids ,rejected, image):
 
             cX = int((topLeft[0] + bottomRight[0])/2.0)
             cY = int((topLeft[1] + bottomRight[1])/2.0)
+    
+            cv2.putText(image, str(markerID),(cX,cY),cv2.FONT_ITALIC,1,(0,0,255),cv2.LINE_4)
             cv2.circle(image,(cX,cY),4,(0,0,255),-1)
 
-    return image
+            if markerID == 0: # Draw Origin
+                cv2.putText(image, "Origin",(topRight),cv2.FONT_ITALIC,1,(255,0,0),cv2.LINE_4)
+                cv2.putText(image, f'{topRight[0], topRight[1]}',(topRight[0]+10,topRight[1]+10),cv2.FONT_ITALIC,1,(255,0,0),cv2.LINE_4)
+                cv2.circle(image,(topRight),6,(255,0,0),-1)
+
 
 if __name__ == '__main__':
 
-    arucoDict = aruco.getPredefinedDictionary(ARUCO_DICT["DICT_5X5_50"])
+    arucoDict = aruco.getPredefinedDictionary(ARUCO_DICT["DICT_6X6_50"])
     detectorParams = aruco.DetectorParameters()
     detector = aruco.ArucoDetector(arucoDict,detectorParams)
-
-    cap = cv2.VideoCapture(0)
+    
+    web_cam = 0
+    usb_camera = 2 
+    cap = cv2.VideoCapture(usb_camera)
     while cap.isOpened():
         ret, img = cap.read()
-
         h,w,_ = img.shape
         width = 1000
         height = int(width*(h/w))
         img = cv2.resize(img,(width,height), interpolation=cv2.INTER_CUBIC)
         corners ,ids, rejected = detector.detectMarkers(img)
 
-        detected_markers = aruco_display(corners,ids,rejected,img)
+        aruco_display(corners,ids,rejected,img)
         
-        cv2.imshow("Result",detected_markers)
+        cv2.imshow("Result",img)
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
--- arucopose.py
+++ arucopose.py
@@ -1,47 +1,110 @@
 import cv2
 import cv2.aruco as aruco 
 import numpy as np 
+import scipy 
 from arucodetect import aruco_display
-from scipy.spatial.transform import Rotation
-from collections import defaultdict
-ARUCO_DICT =  {
+from numpy.linalg import norm 
 
-    "DICT_4X4_50" : cv2.aruco.DICT_4X4_50,
-    "DICT_5X5_50" : cv2.aruco.DICT_5X5_50,
-    "DICT_5X5_100" : cv2.aruco.DICT_5X5_100
-    
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
 }
 
-marker_length = 0.05 # unit : m ... 50mm
+marker_length = 0.099 # unit : m ... 50mm
 
-def pose_estimation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients):
+def camera_pose_estimation(corners,ids, matrix_coefficients, distortion_coefficients):
+
+    """
+    Estimate the camera pos in the World coordinates which the Origin is 
+    Right top corner of ID:0
+
+    This methods follows those steps.
+
+    1) Find the Origin corner 
+        *constraints* 
+        aruco.detectMarkers returns corners and ids.
+        Those are unordered and not able to make sure that detect the origin points. So some tricks are needed.
+
+        1-1) 
+        To run the code in a robust way, get a first-glanced corner 
+        use the fact that each tag sqaures has same gap.
+
+    2) Using aruco.estimatePoseSingleMarkers, get rvec and tvecs (marker to camera)
+
+    3) returned rvec and tvec is the camera pose in the 3D world coordinates
     
-    global rvec
-    global tvec
-    gray =cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    arucoDict = aruco.getPredefinedDictionary(aruco_dict_type)
-    detectorParams = aruco.DetectorParameters()
-    detector = aruco.ArucoDetector(arucoDict,detectorParams)
+    """
+    # if ids is not None:
+    #     # Find the first captured id number
+    #     first_captured_id = int(ids[0])
 
-    corners ,ids, rejected = detector.detectMarkers(gray)
-    rvecs = []
-    tvecs = []
-    if len(corners):
-        for i in range(0,len(ids)):
-          
-            rvec,tvec, _ = aruco.estimatePoseSingleMarkers(corners[i], marker_length, matrix_coefficients,
-                                                                        distortion_coefficients)
-            
-            #aruco.drawdetectedMarkers(frame,corners)
+    #     # Calculate how far from the origin to compensate
+    #     dx, dy  = int(first_captured_id/5), first_captured_id%5
 
-            detected_markers = aruco_display(corners,ids,rejected,img)
-            #aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec)
-            rvecs.append(rvec)
-            tvecs.append(tvec)
-    return frame, rvecs, tvecs
+    #     # Calculate the length of square
+    #     scale = norm(corners[0][0][0]-corners[0][0][1],2)
 
+    #     # Make compensate matrix to translate the corners from the first captured to the origin
+    #     compensate = np.array([[dx*scale, dy*scale],[dx*scale, dy*scale],[dx*scale, dy*scale],[dx*scale, dy*scale]])
 
-arucoDict = aruco.getPredefinedDictionary(ARUCO_DICT["DICT_5X5_50"])
+    #     # Calculate origin corner by substracting first captured corners and compensate matrix 
+    #     origin_corners= corners[0] - compensate
+        
+    #     # Get camera pose 
+    #     rvec, tvec, pts= aruco.estimatePoseSingleMarkers(origin_corners, marker_length, matrix_coefficients, distortion_coefficients)
+        
+    #     # Get estimated origin
+    #     origin = tuple(origin_corners[0][0])
+        
+    #     return rvec.flatten(), tvec.flatten(), origin
+    # elif ids[0] == 0:
+    #     # If first captured  ID is 0, you dont have to compensation step. Just estimate pose right away
+
+    #     rvec, tvec, pts= aruco.estimatePoseSingleMarkers(corners[0], marker_length, matrix_coefficients, distortion_coefficients)
+
+    #     origin = corners[0][0]
+    #     return rvec.flatten(), tvec.flatten(), origin
+    # elif ids is None:
+    #     return None, None, None 
+    if ids is None:
+        return None, None, None 
+    elif [0] in ids:
+        index_id_zero = np.where(ids == 0)[0][0]
+        
+        corner_id_zero = corners[index_id_zero]
+        rvec, tvec, pts= aruco.estimatePoseSingleMarkers(corner_id_zero, marker_length, matrix_coefficients, distortion_coefficients)
+        
+        origin = corner_id_zero[0][0]
+        return rvec.flatten(), tvec.flatten(), origin
+    else:
+        return None, None, None 
+    
+    
+    
+    
+
+    
+        
+
+    
+
+aruco_dict = ARUCO_DICT["DICT_6X6_100"]
+arucoDict = aruco.getPredefinedDictionary(aruco_dict)
 detectorParams = aruco.DetectorParameters()
 detector = aruco.ArucoDetector(arucoDict,detectorParams)
 
@@ -54,28 +117,52 @@ dist = np.array((
  -6.822152395057791e-07))
 
 
-# Camera pose Estimation with ArUco tags.
+# Camera pose Estimation witqh ArUco tags.
 rvecs = []
 tvecs = []
-cap =cv2.VideoCapture(0)
 
+# Cofig Camera
+usb_camera = 2
+cap =cv2.VideoCapture(usb_camera)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 
-while cap.isOpened():
-    ret, img = cap.read()
-    
-    output, rvec, tvec = pose_estimation(img, ARUCO_DICT["DICT_5X5_50"], intrinsic_camera, dist)
+# init ArUco
+arucoDict = aruco.getPredefinedDictionary(aruco_dict)
+detectorParams = aruco.DetectorParameters()
+detector = aruco.ArucoDetector(arucoDict,detectorParams)
 
-    cv2.imshow("Estimated Pose", output)
+# While camera is opened 
+while cap.isOpened():
+    # Capture th videdo
+    _, img = cap.read()
+
+    # Convert images to gray 
+    gray =cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect ArUco markers 
+    corners ,ids, rejected = detector.detectMarkers(gray)    
     
+    # recoginize Origin points and return camera pose and origin 
+    rvec,tvec, origin  = camera_pose_estimation(corners,ids, intrinsic_camera, dist)
+
+    print(rvec)
     print(tvec)
+    # visualize detected tags and origin
+    aruco_display(corners, ids, rejected, img)
+
+    # visualize origin 
+    if origin is not None:
+        cv2.putText(img,"Estimated Origin",(int(origin[0]),int(origin[1])),cv2.FONT_ITALIC,1,(255,100,0),cv2.LINE_8)
+        cv2.circle(img,(int(origin[0]),int(origin[1])),6,(255,100,0),-1)
+    else : 
+        cv2.putText(img,"Estimated Origin",(400,20),cv2.FONT_ITALIC,1,(255,100,0),cv2.LINE_8)
+    cv2.imshow("result" ,img) 
+
     key =cv2.waitKey(1) & 0xFF 
-    if key == 32: # Space Key 
-        rvecs.append(rvec)
-        tvecs.append(tvec)
     if key == ord('q'):
         break
+    
 
 cv2.destroyAllWindows()
 cap.release()
